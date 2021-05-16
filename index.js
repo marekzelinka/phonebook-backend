d930f4b530081999a5ea7bf14ae825302de60475
Marek Zelinka <21342072+marekzelinka@users.noreply.github.com>
2021-05-16 16:45:11 +0200

    finish step2

--- index.js
+++ index.js
@@ -25,15 +25,6 @@ app.use(
   )
 )
 
-app.get('/info', (_req, res) =>
-  res.send(
-    `<div>
-      <p>Phonebook has info for ${persons.length} people</p>
-      <p>${new Date()}</p>
-    </div>`
-  )
-)
-
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
@@ -47,20 +38,12 @@ app.post('/api/persons', (req, res) => {
     return
   }
 
-  const nameExists = persons.find((person) => person.name === body.name)
-  if (nameExists !== undefined) {
-    res.status(400).json({ error: 'name already exists' })
-    return
-  }
-
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: generateId(),
-  }
+  })
 
-  persons = persons.concat(person)
-  res.status(201).json(person)
+  person.save().then((savedPerson) => res.status(201).json(savedPerson))
 })
 
 app.get('/api/persons', (_req, res) => {
