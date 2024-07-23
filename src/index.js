829d936b759477182df0aa4786685d4fb0765efc
Marek Zelinka <mzelinka17@gmail.com>
2024-07-23 06:32:50 +0200

    solve ex3.1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('working?')
+import express from 'express'
+
+const PORT = 3000
+
+let persons = [
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+express()
+  .get('/api/persons', (_req, res) => {
+    res.json(persons)
+  })
+  .listen(PORT, () => console.log(`🚀 Live on port ${PORT}`))
